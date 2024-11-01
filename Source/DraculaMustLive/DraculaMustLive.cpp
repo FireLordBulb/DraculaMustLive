c8daaa34e0ca6d4f47b5f8eb492c6b3e4162f4cd
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-11-01 09:58:33 +0100

    Added Unreal Project

---
+++ Source/DraculaMustLive/DraculaMustLive.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DraculaMustLive.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DraculaMustLive, "DraculaMustLive" );
