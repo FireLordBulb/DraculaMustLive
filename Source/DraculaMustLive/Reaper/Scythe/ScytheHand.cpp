cc730db5a3fc2be67760921a6f77f788d12473fc
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-11-03 15:15:37 +0100

    Added basic interface for ScytheHand

--- Source/DraculaMustLive/Reaper/Scythe/ScytheHand.cpp
+++ Source/DraculaMustLive/Reaper/Scythe/ScytheHand.cpp
@@ -1,14 +1,32 @@
 #include "ScytheHand.h"
 
+#include "Scythe.h"
+
 UScytheHand::UScytheHand()
 {
-	
 }
 
 void UScytheHand::BeginPlay()
 {
+	Scythe = GetWorld()->SpawnActor<AScythe>(ScytheClass, GetComponentTransform());
+	Scythe->AttachToComponent(this, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, false));
 	Super::BeginPlay();
+}
 
-	// ...
-	
+void UScytheHand::OnInputStarted()
+{
+	// TODO: Forward to comboClick start
+	Scythe = &*Scythe;
+}
+
+void UScytheHand::OnInputCompleted()
+{
+	// TODO: Forward to throw/recall start
+	Scythe = &*Scythe;
+}
+
+void UScytheHand::OnInputOngoing(const float ElapsedTime)
+{
+	// TODO: implement bHoldIsActive setting
+	bHoldIsActive = !!bHoldIsActive;
 }
--- Source/DraculaMustLive/Reaper/Scythe/ScytheHand.h
+++ Source/DraculaMustLive/Reaper/Scythe/ScytheHand.h
@@ -1,19 +1,39 @@
 #pragma once
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
+#include "Components/SceneComponent.h"
 #include "ScytheHand.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class DRACULAMUSTLIVE_API UScytheHand : public UActorComponent
+class AScythe;
+
+UCLASS(ClassGroup=(Custom), Blueprintable, meta=(BlueprintSpawnableComponent))
+class DRACULAMUSTLIVE_API UScytheHand : public USceneComponent
 {
 	GENERATED_BODY()
 
 public:
 	UScytheHand();
 
+	UFUNCTION(BlueprintCallable)
+	AScythe* GetScythe() const
+	{
+		return Scythe;
+	}
 protected:
 	virtual void BeginPlay() override;
 
-public:
-	
+private:
+	UFUNCTION(BlueprintCallable)
+	void OnInputStarted();
+	UFUNCTION(BlueprintCallable)
+	void OnInputCompleted();
+	UFUNCTION(BlueprintCallable)
+	void OnInputOngoing(const float ElapsedTime);
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AScythe> ScytheClass;
+
+	UPROPERTY()
+	AScythe* Scythe = nullptr;
+
+	bool bHoldIsActive = false;
 };
